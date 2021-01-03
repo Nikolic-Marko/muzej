2269c3e7a67026d28f48fd9c8373cc30a62150b1
Nikolic-Marko <nikolicmarko653@gmail.com>
2021-01-03 19:44:38 +0100

    Done project

---
+++ js/index.js
@@ -0,0 +1,33 @@
+function nbPhone() {
+    var navBar = document.getElementById("NB_css_response");
+    if (navBar.style.display == "none") {
+        navBar.style.display = "flex";
+        navBar.style.flexDirection = "column"
+    } else {
+        navBar.style.display = "none";
+    }
+}
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");;
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "flex";
+    dots[slideIndex - 1].className += " active";    
+
+}
